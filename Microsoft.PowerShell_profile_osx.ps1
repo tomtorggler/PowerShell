bc75496bd95be8b4efa12b65e3709aaa3ae3d916
Thomas Torggler <ttor@ttmbp.local>
2016-09-20 20:49:21 +0200

    add PowerShell_profile for OSX

---
+++ Microsoft.PowerShell_profile_osx.ps1
@@ -0,0 +1,5 @@
+# Create a symbolic link for the file 
+# ln -s git/PowerShell/Microsoft.PowerShell_profile_osx.ps1 .config/powershell/Microsoft.PowerShell_profile.ps1
+
+# Update PATH variable for Mac so that I can run most bash commands in PS 
+$env:PATH += ":/usr/local/bin"

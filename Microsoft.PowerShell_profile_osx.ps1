d03e2acbce8c05cc330c827119a4f209956e96d6
Thomas Torggler <ttor@ttmbp.local>
2016-09-20 22:14:41 +0200

    updates profile and readme

--- Microsoft.PowerShell_profile_osx.ps1
+++ Microsoft.PowerShell_profile_osx.ps1
@@ -3,3 +3,33 @@
 
 # Update PATH variable for Mac so that I can run most bash commands in PS 
 $env:PATH += ":/usr/local/bin"
+
+# Test - upadte TAK with "cross-platform" functions
+function Show-HostsFile {
+    [CmdletBinding()]
+    param()
+    if($PSVersionTable.PSEdition -eq "Core") {
+        Write-Verbose "PSEdition is $($PSVersionTable.PSEdition)"
+        Get-Content /etc/hosts
+
+    } else {
+        Write-Verbose "PSEdtion is $($PSVersionTable.PSEdition)"
+        Get-Content C:\Windows\System32\Drivers\etc\hosts    
+    }
+}
+
+function Test-SSLConnection {
+    [CmdletBinding()]
+    param($hostname,$port)
+    if($PSVersionTable.PSEdition -eq "Core") {
+        Write-Verbose "$($PSVersionTable.PSEdition)"
+        Write-Verbose "command is: openssl s_client -connect -join $(($hostname,":",$port))"
+        
+        openssl s_client -connect $(-join ($hostname,":",$port))
+
+    } else {
+        Write-Verbose "PSEdtion is $($PSVersionTable.PSEdition)"
+        Get-Content C:\Windows\System32\Drivers\etc\hosts    
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PowerShell
-A repository for general-purpose scripts that don't fit into any other category.
+A repository for all kinds of scripts and snippets.
+
+# Profiles
+The PowerShell profile files I'm using on my Mac OSX and Windows 10 Machines.
+* OSX: Microsoft.PowerShell_profile_osx.ps1
+* Windows: 

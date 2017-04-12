c6b1fea74077ba9d3656a8bdeba46360152c6c12
tomtorggler <tom@uclab.eu>
2017-04-12 19:36:53 +0200

    adds function to check for github releases

--- Microsoft.PowerShell_profile_osx.ps1
+++ Microsoft.PowerShell_profile_osx.ps1
@@ -3,3 +3,29 @@
 
 # Update PATH variable for Mac so that I can run most bash commands in PS 
 $env:PATH += ":/usr/local/bin"
+
+# import modules for powercli
+function Import-PowerCliModules {
+    Import-Module PowerCLI.Vds, PowerCLI.ViCore
+}
+New-Alias -Name ipcli -Value Import-PowerCliModules
+
+# check if newer release is available on GitHub
+function Test-PSVersionGitHub {
+    try {
+        $Release = Invoke-RestMethod https://github.com/PowerShell/PowerShell/releases.atom -ErrorAction Stop | Select-Object -First 1
+    } catch {
+        Write-Warning "Could not check for new version. $_ `n"
+        break
+    }
+    $GitId = $Release.id -split "/" | Select-Object -Last 1
+    $Download = -join("https://github.com",$Release.link.href)
+    
+    if($GitId -eq  $PSVersionTable.GitCommitId) {
+        Write-Host "$GitId is the latest version. `n" -ForegroundColor Green
+    } else {
+        Write-Host "You are running $($PSVersionTable.GitCommitId) but $GitId is available!" -ForegroundColor Yellow
+        Write-Host "Download from: $Download `n" -ForegroundColor Yellow
+    }
+}
+Test-PSVersionGitHub

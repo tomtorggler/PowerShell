05ffa4fd6ad516cd05b6b9e4ea3541ee8c91cff1
tomtorggler <tomtorggler@users.noreply.github.com>
2017-04-16 18:45:34 +0200

    adds Test-PSVersionGitHub to Profile and Exchange tests

---
+++ Exchange_TimeBasedInboxRule.ps1
@@ -0,0 +1,154 @@
+# connect to exchange online
+# Connect-Exchange -Online -Credential (Get-Credential)
+#
+# update rule daily (scheduled task)
+# Get-InboxRule -Mailbox tom -Identity move_night |
+# Set-InboxRule 
+#-ExceptIfReceivedAfterDate (Get-Date 06:00)
+#-ExceptIfReceivedBeforeDate (Get-Date 15:00)
+#
+#Get-Mailbox tom | New-OffHoursFolder -Name "_offhours" -Verbose
+#Get-Mailbox tom | Get-OffHoursFolder -Name night
+#Get-Mailbox tom | Get-OffHoursRule -Name _offhours
+#Get-Mailbox tom | New-OffHoursRule -Name move_offhours -MoveToFolder _offhours -StartTime 08:00 -EndTime 17:00 -Verbose
+#
+
+function Connect-Exchange
+{
+    [CmdletBinding()]
+    Param
+    (
+        # Credential used for connection
+        [Parameter(Mandatory=$true)]
+        [pscredential]
+        $Credential
+    )
+        $params = @{
+            ConnectionUri = "https://outlook.office365.com/powershell-liveid/";
+            ConfigurationName = "Microsoft.Exchange";
+            Credential = $Credential;
+            Authentication = "Basic";
+            AllowRedirection = $true;
+        }
+    try {
+        Write-Verbose "Trying to connect to $($params.ConnectionUri)"
+        $sExch = New-PSSession @params -ErrorAction Stop -ErrorVariable ExchangeSessionError -Verbose:$false
+	    Import-PSSession $sExch
+    } catch {
+        Write-Warning "Could not connect to Exchange $($ExchangeSessionError.ErrorRecord)"
+    }
+}
+
+# Returns Inbox Rule for a Mailbox
+# If rulename or mailbox not found, returns nothing
+function Get-OffHoursRule {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Mailbox,
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Name
+    )
+    process {
+        Get-InboxRule -Mailbox $mailbox -Identity $Name -ErrorAction SilentlyContinue
+    }
+}
+
+function New-OffHoursRule {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        $Mailbox,
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Name,
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $MoveToFolder,
+        $StartTime,
+        $EndTime
+    )
+    process {
+        # Check if target folder already exists
+        $mbxFolder = Get-OffHoursFolder -Mailbox $Mailbox -Name $MoveToFolder
+        # Create folder if doesn't exist
+        if(-not($mbxFolder)){
+            $mbxFolder = New-OffHoursFolder -Mailbox $Mailbox -Name $MoveToFolder
+        }
+        # create folder id string in format: mailbox:\foldername
+        $folderId = $Mailbox.Identity,$mbxFolder.Name -join ":\"
+        Write-Verbose "Folder Id is $folderId"
+        # Create Rule
+        if(-not(Get-OffHoursRule -Mailbox $Mailbox -Name $Name)){
+            $params = @{
+                Mailbox = $Mailbox;
+                Name = $Name;
+                ExceptIfReceivedAfterDate = (Get-Date $StartTime).ToUniversalTime();
+                ExceptIfReceivedBeforeDate = (Get-Date $EndTime).ToUniversalTime();
+                MoveToFolder = $folderId;
+            }
+            Write-Verbose "Create Rule $Name in Mailbox $Mailbox"
+            Write-Verbose "Move mails before $StartTime and after $EndTime to folder: $FolderId"
+            New-InboxRule @params   
+        }
+    }
+}
+
+function Update-OffHoursRule {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        $Mailbox,
+        $Name,
+        $StartTime,
+        $EndTime
+    )
+    process {
+        $params = @{
+            ExceptIfReceivedAfterDate = (Get-Date $StartTime).ToUniversalTime();
+            ExceptIfReceivedBeforeDate = (Get-Date $EndTime).ToUniversalTime();
+        }
+        Get-OffHoursRule -Mailbox $Mailbox -Name $Name | Set-InboxRule @params
+    }
+}
+
+function Get-OffHoursFolder {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Mailbox,
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Name
+    )
+    process {
+        Get-MailboxFolder -Identity "$Mailbox`:\$Name" -ErrorAction SilentlyContinue
+    }
+}
+
+function New-OffHoursFolder {
+    [CmdletBinding()]
+    param (
+        [Parameter(ValueFromPipeline=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Mailbox,
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Name
+    )
+    process {
+        if(Get-MailboxFolder -Identity "$Mailbox`:\$Name" -ErrorAction SilentlyContinue) {
+            Write-Verbose "Folder $Name exists in Mailbox $($Mailbox.Identity)"
+        } else {
+            New-MailboxFolder -Parent $Mailbox -Name $Name
+        }
+    }
+}
--- Microsoft.PowerShell_profile_osx.ps1
+++ Microsoft.PowerShell_profile_osx.ps1
@@ -1,5 +1,9 @@
 # Create a symbolic link for the file 
+# mac
 # ln -s /Users/ttor/git/PowerShell/Microsoft.PowerShell_profile_osx.ps1 /Users/ttor/.config/powershell/Microsoft.PowerShell_profile.ps1
+# bash on win 10
+# mkdir /home/tomt/.config/powershell
+# Copy-Item /mnt/c/Users/Thomas/OneDrive/_psscripts/git/PowerShell/Microsoft.PowerShell_profile_osx.ps1 $Profile -Force
 
 # Update PATH variable for Mac so that I can run most bash commands in PS 
 $env:PATH += ":/usr/local/bin"
@@ -10,28 +14,25 @@ function Import-PowerCliModules {
 }
 New-Alias -Name ipcli -Value Import-PowerCliModules
 
-# check if newer release is available on GitHub
+# check if a new release of PowerShell Core is available on GitHub
 function Test-PSVersionGitHub {
-    [cmdletbinding()]
-    param()
     try {
+        # get latest release from github atom feed
         $Release = Invoke-RestMethod https://github.com/PowerShell/PowerShell/releases.atom -ErrorAction Stop | Select-Object -First 1
     } catch {
         Write-Warning "Could not check for new version. $_ `n"
         break
     }
+    # extract information from atom response
     $GitId = $Release.id -split "/" | Select-Object -Last 1
     $Download = -join("https://github.com",$Release.link.href)
-    
-    if($GitId -eq  $PSVersionTable.GitCommitId) {
-        Write-Verbose "$GitId is the latest version."
-    } else {
-        Write-Verbose "You are running $($PSVersionTable.GitCommitId) but $GitId is available!"
+    # Add information to dictionary for output
+    $output = [ordered]@{
+        "PSVersion" = $PSVersionTable.PSVersion;
+        "GitCommitId" = $PSVersionTable.GitCommitId;
+        "GitHubReleaseVersion" = $GitId;
+        "GitHubReleaseLink" = $Download;
     }
-    New-Object -TypeName psobject -Property ([ordered]@{
-        InstalledVersion=$($PSVersionTable.GitCommitId);
-        GitHubVersion=$GitId;
-        RealeaseLink=$Download
-    })
+    Write-Output (New-Object -TypeName psobject -Property $output)
 }
 Test-PSVersionGitHub

db1cf44f138fa27fbbaff8a66cbb352508671da6
tomtorggler <tom@uclab.eu>
2018-04-30 21:19:08 +0200

    adds profile and updates crypto tools

--- CryptoTools.ps1
+++ CryptoTools.ps1
@@ -34,9 +34,134 @@ function Convert-BchAddress {
         Write-Warning "Could not connect."
     }
     $out | Add-Member -MemberType NoteProperty -Name "Blockchair" -Value "https://blockchair.com/bitcoin-cash/address/$($out.cashaddr.replace(":","%3A"))"
-    $out | Add-Member -MemberType NoteProperty -Name "Blockdozer" -Value "https://blockdozer.com/insight/address/$($out.cashaddr.replace(":","%3A"))"
+    $out | Add-Member -MemberType NoteProperty -Name "Blockdozer" -Value "https://blockdozer.com/address/$($out.cashaddr.replace(":","%3A"))"
     Write-Output $out
 }
 
+enum BNBSymbol {
+    # update with
+    # (irm https://api.binance.com/api/v1/exchangeInfo | select -ExpandProperty symbols | select -ExpandProperty symbol) -join "; "
+    ETHBTC; LTCBTC; BNBBTC; NEOBTC; QTUMETH; EOSETH; SNTETH; BNTETH; BCCBTC; GASBTC; BNBETH; BTCUSDT; ETHUSDT; HSRBTC; OAXETH; DNTETH; MCOETH; ICNETH; MCOBTC; WTCBTC; WTCETH; LRCBTC; LRCETH; QTUMBTC; YOYOBTC; OMGBTC; OMGETH; ZRXBTC; ZRXETH; STRATBTC; STRATETH; SNGLSBTC; SNGLSETH; BQXBTC; BQXETH; KNCBTC; KNCETH; FUNBTC; FUNETH; SNMBTC; SNMETH; NEOETH; IOTABTC; IOTAETH; LINKBTC; LINKETH; XVGBTC; XVGETH; SALTBTC; SALTETH; MDABTC; MDAETH; MTLBTC; MTLETH; SUBBTC; SUBETH; EOSBTC; SNTBTC; ETCETH; ETCBTC; MTHBTC; MTHETH; ENGBTC; ENGETH; DNTBTC; ZECBTC; ZECETH; BNTBTC; ASTBTC; ASTETH; DASHBTC; DASHETH; OAXBTC; ICNBTC; BTGBTC; BTGETH; EVXBTC; EVXETH; REQBTC; REQETH; VIBBTC; VIBETH; HSRETH; TRXBTC; TRXETH; POWRBTC; POWRETH; ARKBTC; ARKETH; YOYOETH; XRPBTC; XRPETH; MODBTC; MODETH; ENJBTC; ENJETH; STORJBTC; STORJETH; BNBUSDT; VENBNB; YOYOBNB; POWRBNB; VENBTC; VENETH; KMDBTC; KMDETH; NULSBNB; RCNBTC; RCNETH; RCNBNB; NULSBTC; NULSETH; RDNBTC; RDNETH; RDNBNB; XMRBTC; XMRETH; DLTBNB; WTCBNB; DLTBTC; DLTETH; AMBBTC; AMBETH; AMBBNB; BCCETH; BCCUSDT; BCCBNB; BATBTC; BATETH; BATBNB; BCPTBTC; BCPTETH; BCPTBNB; ARNBTC; ARNETH; GVTBTC; GVTETH; CDTBTC; CDTETH; GXSBTC; GXSETH; NEOUSDT; NEOBNB; POEBTC; POEETH; QSPBTC; QSPETH; QSPBNB; BTSBTC; BTSETH; BTSBNB; XZCBTC; XZCETH; XZCBNB; LSKBTC; LSKETH; LSKBNB; TNTBTC; TNTETH; FUELBTC; FUELETH; MANABTC; MANAETH; BCDBTC; BCDETH; DGDBTC; DGDETH; IOTABNB; ADXBTC; ADXETH; ADXBNB; ADABTC; ADAETH; PPTBTC; PPTETH; CMTBTC; CMTETH; CMTBNB; XLMBTC; XLMETH; XLMBNB; CNDBTC; CNDETH; CNDBNB; LENDBTC; LENDETH; WABIBTC; WABIETH; WABIBNB; LTCETH; LTCUSDT; LTCBNB; TNBBTC; TNBETH; WAVESBTC; WAVESETH; WAVESBNB; GTOBTC; GTOETH; GTOBNB; ICXBTC; ICXETH; ICXBNB; OSTBTC; OSTETH; OSTBNB; ELFBTC; ELFETH; AIONBTC; AIONETH; AIONBNB; NEBLBTC; NEBLETH; NEBLBNB; BRDBTC; BRDETH; BRDBNB; MCOBNB; EDOBTC; EDOETH; WINGSBTC; WINGSETH; NAVBTC; NAVETH; NAVBNB; LUNBTC; LUNETH; TRIGBTC; TRIGETH; TRIGBNB; APPCBTC; APPCETH; APPCBNB; VIBEBTC; VIBEETH; RLCBTC; RLCETH; RLCBNB; INSBTC; INSETH; PIVXBTC; PIVXETH; PIVXBNB; IOSTBTC; IOSTETH; CHATBTC; CHATETH; STEEMBTC; STEEMETH; STEEMBNB; NANOBTC; NANOETH; NANOBNB; VIABTC; VIAETH; VIABNB; BLZBTC; BLZETH; BLZBNB; AEBTC; AEETH; AEBNB; RPXBTC; RPXETH; RPXBNB; NCASHBTC; NCASHETH; NCASHBNB; POABTC; POAETH; POABNB; ZILBTC; ZILETH; ZILBNB; ONTBTC; ONTETH; ONTBNB; STORMBTC; STORMETH; STORMBNB; QTUMBNB; QTUMUSDT; XEMBTC; XEMETH; XEMBNB; WANBTC; WANETH; WANBNB; WPRBTC; WPRETH; QLCBTC; QLCETH; SYSBTC; SYSETH; SYSBNB; QLCBNB; GRSBTC; GRSETH; ADAUSDT; ADABNB; CLOAKBTC; CLOAKETH; GNTBTC; GNTETH; GNTBNB
+}
 
+
+class BNBTicker {
+
+    BNBTicker($obj) {
+        $this.Symbol = $obj.symbol
+        $this.PercentChange = $obj.pricechangepercent
+        $this.LastPrice = $obj.lastprice
+        $this.Open = ConvertFrom-BNBTime($obj.openTime)
+    }
+
+    [void]FromBNBTime ($MilliSeconds) {
+        $this.Open = ConvertFrom-BNBTime($MilliSeconds)
+    }
+
+    [BNBSymbol]$Symbol
+    [single]$PercentChange
+    [string]$LastPrice
+    [datetime]$Open
+}
+
+function Get-BinanceTicker {
+    [cmdletbinding()]
+    param(
+        # Specify the currency pair (symbol) for which to get the price.
+        [BNBSymbol[]]
+        $Symbol
+    )
+    process {
+        foreach($s in $Symbol){
+            $result = Invoke-RestMethod -Uri https://api.binance.com/api/v1/ticker/24hr?symbol=$s -ErrorAction SilentlyContinue 
+            #| Select-Object -Property symbol,pricechangepercent,lastprice,openTime
+            if($result){
+                [BNBTicker]::new($result)
+            }
+        }   
+    }
+}
+
+function ConvertFrom-BNBTime ($MilliSeconds) {
+    [timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1970-01-01').AddMilliSeconds($MilliSeconds))
+}
+
+
+enum CryptoCurrency {
+    eth
+    btc
+    ltc
+}
+
+function Get-BlockCypherAddress {
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Mandatory=$true, 
+            Position=0
+        )]
+        [string]
+        $Address,
+        [string]
+        $Version = "v1",
+        [CryptoCurrency]
+        $Currency = "eth"
+    )
+
+    $Address = $Address.Replace("0x","")
+
+    Invoke-RestMethod https://api.blockcypher.com/$version/$currency/main/addrs/$Address
+}
+
+function Get-BlockCypherBalance {
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Mandatory=$true, 
+            Position=0
+        )]
+        [string]
+        $Address,
+        [string]
+        $Version = "v1",
+        [CryptoCurrency]
+        $Currency = "eth"
+    )
+    $Address = $Address.Replace("0x","")
+    Invoke-RestMethod https://api.blockcypher.com/$version/$currency/main/addrs/$Address/balance
+}
+
+function Get-BlockCypherTransaction {
+    [CmdletBinding(DefaultParameterSetName="ByTxHash")]
+    param(
+        [Parameter(
+            Position=0,
+            ParameterSetName="ByTxHash",
+            Mandatory=$True
+        )]
+        [string]
+        $Hash,
+        [Parameter(
+            ParameterSetName="ByAddress",
+            Mandatory=$True
+        )]
+        [string]
+        $Address,
+        [string]
+        $Version = "v1",
+        [CryptoCurrency]
+        $Currency = "eth"
+    )
+
+    # provide two possibilites of using the function, 
+    # either get a specific tx by hash or get all tx for a certain address
+
+    if($Hash) {
+        $hash = $hash.Replace("^0x","")
+        Invoke-RestMethod https://api.blockcypher.com/$version/$currency/main/txs/$Hash
+    } elseif ($Address) {
+        $Address = $Address.Replace("0x","")
+        Get-BlockCypherAddress -Address $Address -Currency $PSBoundParameters.Currency | Select-Object -ExpandProperty txrefs
+    }
+}
 
---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,220 @@
+#######
+### The actual $PROFILE only contains the following line
+### . $env:USERPROFILE\Git\PowerShell\Microsoft.PowerShell_profile.ps1
+### to dot-source this file
+######
+
+## Aliases... because of muscle memory :/
+New-Alias -Name ll -Value Get-ChildItem -ErrorAction SilentlyContinue
+
+Import-Module C:\Users\thomas.torggler\AppData\Local\Apps\2.0\9V9HVHDB.2KG\691XWNER.8NB\micr..tion_d8f8f667ee342b5c_0010.0000_6b4a13fd451b1c00\Microsoft.Exchange.Management.ExoPowershellModule.dll -ErrorAction SilentlyContinue
+
+function Invoke-AsAdmin {
+    param($command)
+    Start-Process powershell -Verb RunAs -ArgumentList "-noprofile $command"
+}
+
+function Set-DnsOpen { 
+    param(
+        $InterfaceIndex = 4
+    )
+    $command = "Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses ('208.67.222.222','208.67.220.220','2620:0:ccc::2','2620:0:ccd::2')"
+    Invoke-AsAdmin $command
+}
+
+function Set-DnsDhcp {
+    param(
+        $InterfaceIndex = 4
+    )
+    $command = "Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ResetServerAddresses"
+    Invoke-AsAdmin $command
+}
+
+function Get-InternetProxy {
+    param(
+        [switch]$ShowAutoConfig
+    )
+    $regKey="HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"        
+    Get-ItemProperty -path $regKey | Select-Object -Property ProxyEnable,ProxyServer,ProxyOverride,AutoConfigURL,@{n="AutoDetect";E={Get-InternetProxyAutoDetect}}
+    if($ShowAutoConfig) {
+        $path = Get-ItemProperty -path $regKey | Select-Object -ExpandProperty AutoConfigURL
+        if($path -match "file:") {
+            Get-Content -Path $($path.replace("file://",""))
+        }
+    }               
+}
+Function Disable-InternetProxy {
+    param(
+        [switch]$ClearAutoConfigUrl
+    )
+    $regKey="HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"        
+    Set-ItemProperty -Path $regKey -Name ProxyEnable -Value 0
+    Set-ItemProperty -Path $regKey -Name ProxyServer -Value ""
+    if ($ClearAutoConfigUrl0) {
+        Set-ItemProperty -Path $regKey -Name AutoConfigURL -Value ""
+    }               
+}
+Function Enable-InternetProxy {
+    param(
+        $proxyServer = "localhost:8118",
+        $AutoConfigFile
+    )
+    $regKey="HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"        
+    Set-ItemProperty -Path $regKey -Name ProxyEnable -Value 1
+    Set-ItemProperty -Path $regKey -Name ProxyServer -Value $proxyServer
+    if($AutoConfigFile) {
+        Set-ItemProperty -Path $regKey -Name AutoConfigURL -Value $AutoConfigFile
+    }
+}
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+function Set-SecurityProtocol {
+    param($Protocol,[switch]$Default)
+    if($default) {
+        [net.servicepointmanager]::SecurityProtocol = [System.Security.Authentication.SslProtocols]::Default
+    } else {
+        [net.servicepointmanager]::SecurityProtocol = $Protocol
+    }
+}
+function Get-SecurityProtocol {
+    [net.servicepointmanager]::SecurityProtocol
+}
+
+if($PSEdition -ne "Core"){
+    $connectedIf = Get-NetIPInterface -ConnectionState Connected | Where-Object ifIndex -ne 1
+    $dnsServers = (Get-DnsClientServerAddress -InterfaceIndex $connectedIf.IfIndex | select -ExpandProperty serveraddresses) -join ", "
+}
+$write = "
+---
+Hello $($env:USERNAME) @ $($env:COMPUTERNAME) [$($PSVersionTable.BuildVersion)]
+
+Remeber: Set-DnsOpen, Set-DnsDhcp, Get-DnsClientServerAddress, Get-InternetProxy
+HidServ: zik25npqnzrdl3r7.onion 6slsw42zq5n76gt7.onion uhhikmyycppwnh3b.onion
+
+Connection Alias $(($connectedIf.InterfaceAlias | Select-Object -Unique) -join ", ") 
+Connection Index $(($connectedIf.IfIndex | Select-Object -Unique) -join ", ")
+DNS Servers: $dnsServers
+Security Protcols: $(Get-SecurityProtocol)
+---
+"
+Write-Host $write -ForegroundColor Yellow
+
+
+function Send-MailJetMail {
+    [cmdletbinding()]
+	param(
+        [string]$Sender = "notification@tomt.it",
+        [string]$Recipient,
+        [string]$Subject,
+        [string]$Text,
+        [string]$ApiKey,
+        [string]$Secret
+    )
+    $body = @{
+        Messages = @(@{
+            From = @{
+                Email = $sender
+                Name = "Notification"
+            }
+            To = @(@{
+                Email = $recipient
+                Name = ""
+            })
+            Subject = $subject
+            TextPart = $text
+        })
+    
+    } | ConvertTo-Json -Depth 4
+    $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $ApiKey,$Secret)))
+	$Result = Invoke-RestMethod -Uri "https://api.mailjet.com/v3.1/send" -ContentType "application/json" -body $body -Method POST -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo)} -UseBasicParsing
+    if($Result) {
+        $Result.Messages
+    }
+}
+
+
+function prompt 
+{ 
+    $pss = Get-PSSession
+    $cwd = (Get-Location).Path        
+    if($pss) {
+        $WindowTitle = "Connected to: " + ($pss.ComputerName -join ", ") 
+        $info = $pss.Name -join ", " 
+    } else {
+        $WindowTitle = $cwd
+        $info = "PS"
+    }
+    $host.UI.RawUI.WindowTitle = $WindowTitle
+    $host.UI.Write("Yellow", $host.UI.RawUI.BackGroundColor, "[$info]")
+    " $($cwd.Replace($env:USERPROFILE,"~"))$('>' * ($nestedPromptLevel + 1)) ";
+}
+
+
+function Get-InternetProxyAutoDetect {
+    [CmdletBinding()]
+    param()
+
+    $RegKey = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Connections\" 
+    $DefaultConnection = $(Get-ItemProperty $RegKey).DefaultConnectionSettings 
+
+    if ($($DefaultConnection[8] -band 8) -ne 8) { 
+        Write-Verbose "Auto Detection disabled for Default Connection"
+        Write-Output $false 
+    } else { 
+        Write-Verbose "Auto Detection enabled for Default Connection"
+        Write-Output $true 
+    }
+}
+
+function Enable-InternetProxyAutoDetect {
+    [CmdletBinding()]
+    param()
+
+    $RegKey = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Connections\" 
+    $DefaultConnection = $(Get-ItemProperty $RegKey).DefaultConnectionSettings 
+
+    $DefaultConnection[8] = $DefaultConnection[8] -bor 8 
+    $DefaultConnection[4]++ 
+
+    Write-Verbose "Enabling Proxy auto detection for Default Connection"
+    Set-ItemProperty -Path $RegKey -Name DefaultConnectionSettings -Value $DefaultConnection 
+}
+
+function Disable-InternetProxyAutoDetect {
+    [CmdletBinding()]
+    param()
+
+    $RegKey = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings\Connections\" 
+    $DefaultConnection = $(Get-ItemProperty $RegKey).DefaultConnectionSettings 
+
+    $mask = -bnot 8 
+    $DefaultConnection[8] = $DefaultConnection[8] -band $mask 
+    $DefaultConnection[4]++ 
+    
+    Write-Verbose "Disabling Proxy auto detection for Default Connection"
+    Set-ItemProperty -Path $RegKey -Name DefaultConnectionSettings -Value $DefaultConnection 
+}
+
+function Get-PublicIP {
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [ValidateRange(1,8)]
+        [int]
+        $Timeout = 1
+    )
+    # why does irm output an error message even if silentlycontinue?
+    [ipaddress]$ip4 = Invoke-RestMethod http://ipv4.myip.dk/api/Info/IPv4Address -TimeoutSec $Timeout -ErrorAction SilentlyContinue
+    [ipaddress]$ip6 = Invoke-RestMethod http://ipv6.myip.dk/api/Info/IPv6Address -TimeoutSec $Timeout -ErrorAction SilentlyContinue
+    $out = @{
+        IPv4 = $ip4.IPAddressToString
+    }
+    if ($ip6.AddressFamily -eq "InterNetworkV6"){
+        $out.add("IPv6",$ip6.IPAddressToString)
+    }
+    New-Object -TypeName psobject -Property $out
+}

a11c3156da2eb927894fde06c47ddfaa8b16edfa
Tom Torggler <tom@uclab.eu>
2023-06-21 14:50:27 +0200

    update

--- bv-calc.ps1
+++ bv-calc.ps1
@@ -1,20 +1,36 @@
 
-$path = Join-Path $HOME -ChildPath 'OneDrive/_BV/Experts Inside BV/Q*/Sales Invoice'
-Get-ChildItem $path -Filter *.xlsx -Recurse | ForEach-Object { 
-    $n = $_.basename.Split('-')[0]
-    $e = Import-Excel -Path $_.fullname  -NoHeader
-    $c = $e | Where-Object p8 -eq 'Due Upon Receipt' | Select-Object -expand p1
-    $t = $e | Where-Object p6 -eq total | Select-Object -expand p8
-    [pscustomobject]@{
-        Invoice = $n
-        Customer = $c
-        Value = $t
-    }  
-} | Group-Object Customer | Select-Object Name,@{
-    n='sum'
-    e={($_.group.value | Measure-Object -sum).Sum}
+function Get-InvoiceDetail {
+    param(
+        $year = [datetime]::UtcNow.Year,
+        $quarter = "*"
+    )
+    process {
+        $path = Join-Path $HOME -ChildPath "OneDrive/_BV/Experts Inside BV/$year/Q$quarter/Sales Invoice"
+        Get-ChildItem $path -Filter *.xlsx -Recurse | ForEach-Object { 
+            $n = $_.basename.Split('-')[0]
+            $e = Import-Excel -Path $_.fullname  -NoHeader
+            $c = $e | Where-Object p8 -eq 'Due Upon Receipt' | Select-Object -expand p1
+            $t = $e | Where-Object p6 -eq total | Select-Object -expand p8
+            [pscustomobject]@{
+                Invoice = $n
+                Customer = $c
+                Value = $t
+            }  
+        }
+    }
 }
 
-# | Export-Excel -TableName T1 -PivotTableName p1 -PivotColumns Customer -PivotRows Invoice
+Get-InvoiceDetail
 
+
+Get-InvoiceDetail | Group-Object Customer | Select-Object @{
+    n="Invoice"
+    e={"SUBTOTAL"}
+},@{
+    n='Customer'
+    e={$_.Name}
+},@{
+    n='Value'
+    e={($_.group.value | Measure-Object -sum).Sum}
+}
 
